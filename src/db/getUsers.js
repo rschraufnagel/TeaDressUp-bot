67c34b5bed1fc0a7f4fedc6f92679332a3b63999
jtmchenry <theimaninja@gmail.com>
2018-11-11 21:01:20 -0500

    Adding gets for database tables

---
+++ src/db/getUsers.js
@@ -0,0 +1,16 @@
+const sqlite3 = require('sqlite3').verbose();
+const config = require('../config');
+
+//Retrieve UserId for DressUpItemsOwned
+function selectUser(user) {
+  return new Promise(function(resolve, reject) {
+    let db = new sqlite3.Database(config.connection, (err) => {if (err) {reject(err);}});
+    let sqlSelectUser = "SELECT userid from DiscordUser WHERE username = ?"
+
+    db.run(sqlSelectUser, [user.username], (err) => {
+      if (err) {reject (err);}
+      resolve(user);
+    });
+    db.close();
+  });
+}
